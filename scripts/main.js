79305091fc56a0d575ddef30dcbaf520926f4dbc
ESlingsby406 <20eslingsby@thelangton.org.uk>
2025-09-16 08:43:49 +0100

    changed all the formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,29 @@
-const myImage = document.querySelector("img");
-
-// switches between images when clicked //
-myImage.addEventListener("click", () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/sunrise-image.jpg") {
-        myImage.setAttribute("src", "images/tiger-image.jpg");
-    } else {
-        myImage.setAttribute("src", "images/sunrise-image.jpg");
-    }
-});
-
-let myButton = document.querySelector("button");
+let btn = document.querySelector("btn");
 let myHeading = document.querySelector("h1");
 
-// gets user to enter their name and stores it in a variable //
 function setUserName() {
-    const myName = prompt("please enter your name");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Welcome to my CS website, ${myName}`;
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Welcome to my Computer Science website, ${myName}`;
 }
 
-//initialisation code - runs when page first loads to start program//
-//sets the name in intro message to the inputted name//
 if (!localStorage.getItem("name")) {
-    setUserName();
+  setUserName();
 } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Welcome to my CS website, ${storedName}`;
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome to my Computer Science website, ${storedName}`;
 }
 
-//adds a click feature to allow the user to enter another name if they want//
-myButton.addEventListener("click", () => {
-    setUserName();
+btn.addEventListener("click", () => {
+  setUserName();
 });
 
 function setUserName() {
-    const myName = prompt("please enter your name");
-    if (!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem("name", myName);
-        myHeading.textContent = `Welcome to my CS website, ${myName}`;
-    }
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to my Computer Science website, ${myName}`;
+  }
 }
